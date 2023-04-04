548b8625451829420e0f6536f59895f07934d974
MatheusTrindade5201 <matheus.trindade5201@hotmail.com>
2023-04-03 21:55:24 -0300

    feat: creating a counter component

---
+++ components/product/Counter.tsx
@@ -0,0 +1,31 @@
+import { useSignal } from "@preact/signals";
+
+const Counter = () => {
+  const quantity = useSignal(0);
+
+  const increaseQuantity = () => {
+    quantity.value++;
+  };
+
+  const decreaseQuantity = () => {
+    quantity.value--;
+  };
+
+  return (
+    <div class="flex font-semibold items-center border-2 border-gray-200 rounded-[4px] overflow-hidden">
+      <button
+        class="disabled:(bg-gray-100 text-gray-400) px-0.5 font-semibold text-sm text-orange"
+        disabled={quantity.value === 0}
+        onClick={decreaseQuantity}
+      >
+        －
+      </button>
+      <p class="px-2 border-x-2 h-full text-sm border-gray-200 leading-5">
+        {quantity}
+      </p>
+      <button class="text-sm px-0.5" onClick={increaseQuantity}>＋</button>
+    </div>
+  );
+};
+
+export default Counter;
